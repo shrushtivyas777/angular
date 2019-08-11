8f8dd9bcf7da34a004e5e54896e083dc5010ab12
shrushti <shrushtivyas777@gmail.com>
2019-08-12 00:47:03 +0530

    I created this app

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ApiServiceService } from './api-service.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  constructor(public myService: ApiServiceService){
+  }
+}
